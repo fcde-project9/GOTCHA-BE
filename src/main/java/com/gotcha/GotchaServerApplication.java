84c3e801397a642ad657114438941e685f4a69a1
shawnchoi8 <bychoise8@gmail.com>
2025-12-31 15:05:36 +0900

    first commit

---
+++ src/main/java/com/gotcha/GotchaServerApplication.java
@@ -0,0 +1,13 @@
+package com.gotcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotchaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotchaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gotcha/GotchaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotchaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
